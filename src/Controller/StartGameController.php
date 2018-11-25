22c49e4fef304dce49768756850195d74964b13c
JakubPR <jprochocki@gmail.com>
2018-11-25 22:23:30 +0100

    created base template/controller for game view

--- src/Controller/StartGameController.php
+++ src/Controller/StartGameController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WelcomeController extends AbstractController
+class StartGameController extends AbstractController
 {
     /**
-     * @Route("/", name="welcome")
+     * @Route("/start", name="start-game")
      */
     public function index()
     {
-        return $this->render('welcome/welcome.html.twig');
+        return $this->render('game/game.html.twig');
     }
 }
