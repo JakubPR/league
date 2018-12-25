7f6684fffb54ae1b07b23a4e7429e40ebb0c7d5a
JakubPR <jprochocki@gmail.com>
2018-12-25 22:05:38 +0100

    Points calculation added

--- src/Controller/RenderTableController.php
+++ src/Controller/RenderTableController.php
@@ -13,32 +13,80 @@ class RenderTableController extends AbstractController
 {
     private $repo;
     private $sessionManager;
+    private $tableBuilder;
 
-    public function __construct(ScoreTableRepository $repo, SessionManager $sessionManager)
+    public function __construct(ScoreTableRepository $repo, SessionManager $sessionManager, ScoreTableBuilder $tableBuilder)
     {
         $this->repo = $repo;
         $this->sessionManager = $sessionManager;
+        $this->tableBuilder = $tableBuilder;
     }
 
     /**
      * @Route("/render/table", name="render_table")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(Request $request, ScoreTableBuilder $tableBuilder, SessionManager $sessionManager)
+    public function index(Request $request, SessionManager $sessionManager)
     {
         $tableStatus = $request->get('tableStatus');
-        $tableData = $this->getTableDataForCurrentGame($tableBuilder->getLastAddedGameId());
+        $tableData = $this->getTableDataForCurrentGame($this->tableBuilder->getLastAddedGameId());
         $this->shuffleOnce($tableData);
 
-        $pairsCount = count($sessionManager->getShuffledData());
+        // $pairsCount = count($sessionManager->getShuffledData());
         $pair = $sessionManager->getShuffledData()[0];
-        dump($pair);
 
         return $this->render('render_table/index.html.twig', [
             'tableStatus' => $tableStatus,
             'tableData' => $tableData,
-            'shuffleData' => $sessionManager->getShuffledData()
+            'shuffleData' => $sessionManager->getShuffledData(),
+            'pair' => $pair
         ]);
+    }
+
+    /**
+     * @Route("/render/getscore", name="get_score")
+     **/
+    public function getScore(Request $request)
+    {
+        $results = $request->request->all();
+
+        $i = 1;
+        foreach ($results as $id => $score)
+        {
+            ${"player$i"} = ['id' => $id, 'score' => $score];
+            $i++;
+        }
+
+        $this->setTablePoints($player1, $player2);
+
+        $test= 'test';
+        dump($test);
+    }
+
+    public function setTablePoints($player1, $player2)
+    {
+        if ($player1['score'] > $player2['score']) {
+            $player1 += ['table_points' => 2];
+            $player2 += ['table_points' => -2];
+        } elseif ($player1['score'] < $player2['score']) {
+            $player1 += ['table_points' => -2];
+            $player2 += ['table_points' => 2];
+        } else {
+            $player1 += ['table_points' => 1];
+            $player2 += ['table_points' => 1];
+        }
+        dump($player1);
+        dump($player2);
+
+        $this->addPointsToTable($player1);
+
+    }
+
+    function addPointsToTable($player1)
+    {
+        $tableData = $this->getTableDataForCurrentGame($this->tableBuilder->getLastAddedGameId());
+        dump($tableData);
+
     }
 
     public function shuffleOnce($tableData)
