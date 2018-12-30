70b03b15756af3b83e4a55b9ff2ee152476d0255
JakubPR <jprochocki@gmail.com>
2018-12-30 20:41:24 +0100

    set up for number of games

---
+++ src/Controller/NumberOfGamesController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller;
+
+use App\Builder\StatusTable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route as Route;
+
+class NumberOfGamesController extends AbstractController
+{
+    /**
+     * @Route("/numberofgames", name="games-number")
+     */
+    function show()
+    {
+        return $this->render('number of games/index.html.twig');
+    }
+
+    /**
+     * @Route("/numberofgames/getnumber", name="get_number")
+     **/
+    function saveNumberOfGames(Request $request, StatusTable $statusTable)
+    {
+        $statusTable->changeStatus('numberOfGames',$request->get('number'));
+        return $this->redirectToRoute('welcome');
+    }
+}
