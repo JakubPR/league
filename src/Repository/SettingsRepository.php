7ee236b096310a88039252a456f152f8a53b3683
JakubPR <jprochocki@gmail.com>
2019-02-06 03:09:32 +0100

    Update SettingsRepository.php

--- src/Repository/SettingsRepository.php
+++ src/Repository/SettingsRepository.php
@@ -6,12 +6,6 @@ use App\Entity\Settings;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
-/**
- * @method Settings|null find($id, $lockMode = null, $lockVersion = null)
- * @method Settings|null findOneBy(array $criteria, array $orderBy = null)
- * @method Settings[]    findAll()
- * @method Settings[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class SettingsRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
