350d0b29940574cc5e0969aa307273247c2db715
JakubPR <jprochocki@gmail.com>
2019-02-07 22:10:57 +0100

    Preparing points calculation.

--- src/Controller/UpdateTableController.php
+++ src/Controller/UpdateTableController.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route as Route;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Helpers\Calculate;
 
 class UpdateTableController
 {
     /**
      * @Route("/render/table/update/{scores}", name="update_table")
      *
-     * @param string $scores
+     * @param string    $scores
+     * @param Calculate $calc
      */
-    public function updateTable(string $scores, EntityManagerInterface $em)
-    {
-        dump(json_decode($scores));
-
+    public function updateTable(
+        string $scores,
+        Calculate $calc
+    ) {
+        $calc->calculatePoints(json_decode($scores));
     }
 }
---
+++ src/Helpers/Calculate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Helpers;
+
+class Calculate
+{
+    public function calculatePoints(array $scores)
+    {
+
+    }
+}
